3c1b3c0e8632cd045dd7d5944a1e3606bfa70906
SebastianRG16 <89326030+SebastianRG16@users.noreply.github.com>
2024-02-19 23:22:32 -0300

    descriptions, exprience and services

--- src/pages/experience/Experience.jsx
+++ src/pages/experience/Experience.jsx
@@ -1,15 +1,17 @@
+import { useTranslation } from "react-i18next";
+
 export function Experience() {
+  const { t } = useTranslation();
+
   return (
     <div className="w-full">
       <div className="w-full flex flex-col">
         <div className="w-full">
           <div className="w-full flex justify-center text-5xl font-bold text-[#272727] mt-6">
-            <p className="md:w-3/4 text-center">Experiencia laboral como desarrollador de software</p>
+            <p className="md:w-3/4 text-center">{t("titleExperience")}</p>
           </div>
           <div className="mt-16 flex justify-center">
-            <p className="md:w-4/6 text-center">
-            Como desarrollado de software cuento con experiencia y participación en diferentes proyectos y empresas, contribuyendo con el desarrollo y mantenimiento de software, mis habilidades incluyen desarrollar Frontend, Backend y Móvil, llevando una comunicación efectiva y ágil con el equipo.
-            </p>
+            <p className="md:w-4/6 text-center">{t("descriptionExperience")}</p>
           </div>
         </div>
         <section className="mt-16">
@@ -17,9 +19,9 @@ export function Experience() {
             <div className="grid gap-4 sm:mx-4 sm:grid-cols-12">
               <div className="col-span-12 sm:col-span-3">
                 <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:bg-[#123ad3]">
-                  <h3 className="text-3xl font-semibold">Morbi tempor</h3>
+                  <h3 className="text-3xl font-semibold">Trabajos</h3>
                   <span className="text-sm font-bold tracki uppercase text-gray-400">
-                    Vestibulum diam nunc
+                    Sebastian Rodriguez
                   </span>
                 </div>
               </div>
@@ -27,7 +29,7 @@ export function Experience() {
                 <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-700">
                   <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#123ad3]">
                     <h3 className="text-xl font-semibold tracki">
-                      Donec porta enim vel{" "}
+                      InnovarWeb Tecnologies{" "}
                     </h3>
                     <time className="text-xs tracki uppercase text-gray-400">
                       Dec 2020
@@ -40,7 +42,7 @@ export function Experience() {
                   </div>
                   <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#123ad3]">
                     <h3 className="text-xl font-semibold tracki">
-                      Aliquam sit amet nunc ut
+                      Prosoft SC
                     </h3>
                     <time className="text-xs tracki uppercase text-gray-400">
                       Jul 2019
@@ -49,20 +51,6 @@ export function Experience() {
                       Morbi vulputate aliquam libero non dictum. Aliquam sit
                       amet nunc ut diam aliquet tincidunt nec nec dui. Donec
                       mollis turpis eget egestas sodales.
-                    </p>
-                  </div>
-                  <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-[#123ad3]">
-                    <h3 className="text-xl font-semibold tracki">
-                      Pellentesque habitant morbi
-                    </h3>
-                    <time className="text-xs tracki uppercase text-gray-400">
-                      Jan 2016
-                    </time>
-                    <p className="mt-3">
-                      Suspendisse tincidunt, arcu nec faucibus efficitur, justo
-                      velit consectetur nisl, sit amet condimentum lacus orci
-                      nec purus. Mauris quis quam suscipit, vehicula felis id,
-                      vehicula enim.
                     </p>
                   </div>
                 </div>
--- src/pages/services/Seriveces.jsx
+++ src/pages/services/Seriveces.jsx
@@ -3,38 +3,34 @@ import mucenos from "../../assets/images/mucenosGP.png";
 import restaurant from "../../assets/images/restaurant.png";
 import meraki from "../../assets/images/meraki.png";
 import jac from "../../assets/images/jac.png";
+import { useTranslation } from "react-i18next";
 
 export function Servicies() {
+  const { t } = useTranslation();
+
   return (
     <div className="w-full">
       <div className="w-full flex">
         <div className="w-full justify-center text-center flex text-5xl font-bold text-[#272727] mt-6">
-          <p className="lg:w-3/4">Proyectos de mas valor que he desarrollado</p>
+          <p className="lg:w-3/4">{t("titleProjects")}</p>
         </div>
       </div>
       <div className="w-full flex">
         <div className="mt-16 w-full justify-center flex text-center">
-          <p className="lg:w-4/6">
-            He tenido la oportunidad de liderar y contribuir en proyectos de alto valor, centrados en la entrega de soluciones de calidad e innovadoras, asi como la optimizacion de procesos empresariales. 
-          </p>
+          <p className="lg:w-4/6">{t("descriptionProjects")}</p>
         </div>
       </div>
       <div className="border-b-2 border-[#7d7df4] my-4 mt-20 mr-32 ml-32"></div>
       <section className="p-4 lg:p-8">
         <div className="container mx-auto space-y-12">
           <div className="flex shadow-xl bg-gray-100 flex-col overflow-hidden rounded-md shadow-sm lg:flex-row transform transition-transform hover:scale-105 duration-500">
-            <img
-              src={zaperoco}
-              className="h-80  aspect-video"
-            />
+            <img src={zaperoco} className="h-80  aspect-video" />
             <div className="flex flex-col justify-center flex-1 p-6 ">
-              <span className="text-xs uppercase ">Join, it's free</span>
-              <h3 className="text-3xl font-bold">
-                Zaperoco Software
-              </h3>
+              <span className="text-xs uppercase "></span>
+              <h3 className="text-3xl font-bold">Zaperoco Software</h3>
               <p className="my-6 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                aliquam possimus quas, error esse quos.
+                La finalidad de zaperoco software es ayudar a una fundacion de
+                niños con cancer a recaudar donaciones en sus eventos.
               </p>
               <button type="button" className="self-start">
                 Action
@@ -42,18 +38,14 @@ export function Servicies() {
             </div>
           </div>
           <div className="flex shadow-xl bg-gray-100 flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse transform transition-transform hover:scale-105 duration-500">
-            <img
-              src={mucenos}
-              className="h-80  aspect-video"
-            />
+            <img src={mucenos} className="h-80  aspect-video" />
             <div className="flex flex-col justify-center flex-1 p-6 ">
-              <span className="text-xs uppercase ">Join, it's free</span>
-              <h3 className="text-3xl font-bold">
-                Muceños Gastropub
-              </h3>
+              <span className="text-xs uppercase "></span>
+              <h3 className="text-3xl font-bold">Muceños Gastropub</h3>
               <p className="my-6 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                aliquam possimus quas, error esse quos.
+                Software desarrollado para un restaurante con la finalidad de
+                que los clientes interactuen con el sistema y puedan dispensar
+                su serversa haciendo uso de esete sistema interactivo.
               </p>
               <button type="button" className="self-start">
                 Action
@@ -61,18 +53,14 @@ export function Servicies() {
             </div>
           </div>
           <div className="flex shadow-xl bg-gray-100 flex-col overflow-hidden rounded-md shadow-sm lg:flex-row transform transition-transform hover:scale-105 duration-500">
-            <img
-              src={restaurant}
-              className="h-80  aspect-video"
-            />
+            <img src={restaurant} className="h-80  aspect-video" />
             <div className="flex flex-col justify-center flex-1 p-6 ">
-              <span className="text-xs uppercase ">Join, it's free</span>
-              <h3 className="text-3xl font-bold">
-                Smart Restaurant
-              </h3>
+              <span className="text-xs uppercase "></span>
+              <h3 className="text-3xl font-bold">Smart Restaurant</h3>
               <p className="my-6 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                aliquam possimus quas, error esse quos.
+                Es un software que busca automatizar proceso en la industria
+                restaurantera, asi como llevar toda su contabilidad, brindando
+                una nueva experiencia al cliente.
               </p>
               <button type="button" className="self-start">
                 Action
@@ -80,18 +68,14 @@ export function Servicies() {
             </div>
           </div>
           <div className="flex shadow-xl bg-gray-100 flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse transform transition-transform hover:scale-105 duration-500">
-            <img
-              src={meraki}
-              className="h-80  aspect-video"
-            />
+            <img src={meraki} className="h-80  aspect-video" />
             <div className="flex flex-col justify-center flex-1 p-6 ">
-              <span className="text-xs uppercase ">Join, it's free</span>
-              <h3 className="text-3xl font-bold">
-                Meraki Software
-              </h3>
+              <span className="text-xs uppercase "></span>
+              <h3 className="text-3xl font-bold">Meraki Software</h3>
               <p className="my-6 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                aliquam possimus quas, error esse quos.
+                Meraki software es un emprendimiento donde se busca desarrollar
+                software a la medida y de la misma manera desarrollar un
+                software que los caracterice.
               </p>
               <button type="button" className="self-start">
                 Action
@@ -99,18 +83,14 @@ export function Servicies() {
             </div>
           </div>
           <div className="flex shadow-xl bg-gray-100 flex-col overflow-hidden rounded-md shadow-sm lg:flex-row transform transition-transform hover:scale-105 duration-500">
-            <img
-              src={jac}
-              className="h-80  aspect-video"
-            />
+            <img src={jac} className="h-80  aspect-video" />
             <div className="flex flex-col justify-center flex-1 p-6 ">
-              <span className="text-xs uppercase ">Join, it's free</span>
-              <h3 className="text-3xl font-bold">
-                JAC Software
-              </h3>
+              <span className="text-xs uppercase "></span>
+              <h3 className="text-3xl font-bold">JAC Software</h3>
               <p className="my-6 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
-                aliquam possimus quas, error esse quos.
+                JAC es un software creado para una junta de accion comunal, con
+                la finalidad de ayudar en el seguimiento de reuniones,
+                interactividad con la comunidad y administracion de archivos.
               </p>
               <button type="button" className="self-start">
                 Action
